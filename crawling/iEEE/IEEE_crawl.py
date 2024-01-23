3ceadd3e697f49d002074fcc6cf96fed18d2f785
choiseohyeon <csh200310@naver.com>
2024-01-23 10:08:32 +0900

    [feat] 정확한 chrome driver 경로 추가

--- crawling/iEEE/IEEE_crawl.py
+++ crawling/iEEE/IEEE_crawl.py
@@ -4,12 +4,15 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException, NoSuchElementException, ElementClickInterceptedException
+from selenium.webdriver.chrome.service import Service
+
 
 # Chrome WebDriver 경로 설정
-webdriver_path = '/path/to/chromedriver'  # 실제 chromedriver 위치로 수정해주세요.
+webdriver_path = '/Users/choeseohyeon/.cache/selenium/chromedriver/mac-arm64/120.0.6099.109/chromedriver'
+s = Service(webdriver_path)
 
 # 웹 드라이버 초기화
-driver = webdriver.Chrome(executable_path=webdriver_path)
+driver = webdriver.Chrome(service=s)
 
 # 쿠키 팝업을 처리하는 함수
 def handle_cookie_popup():
@@ -27,6 +30,32 @@ def handle_cookie_popup():
     except (ElementClickInterceptedException, NoSuchElementException) as e:
         # 버튼 클릭이 가로막힌 경우 또는 버튼을 찾을 수 없는 경우
         print("쿠키 설정 버튼을 클릭할 수 없습니다:", e)
+
+# 페이지 내용을 크롤링하는 함수
+def crawl_page():
+    # 페이지에 있는 각 항목에 대한 정보를 가져오는 로직을 여기에 작성
+    # 예를 들어, 제목과 요약을 가져오려면 해당 요소를 찾아야 함
+    articles = driver.find_elements(By.XPATH, "//div[@class='List-results-items']")
+    for article in articles:
+        # 제목과 요약을 추출
+        title = article.find_element(By.XPATH, ".//h2").text
+        abstract = article.find_element(By.XPATH, ".//div[@class='description']").text
+        print(title, abstract)
+        # 필요하다면 추출된 데이터를 파일에 저장하거나 다른 처리를 할 수 있음
+
+# 페이지 네비게이션을 처리하는 함수
+def navigate_pages():
+    try:
+        while True:
+            crawl_page()
+            next_button = driver.find_element(By.XPATH, "//a[@aria-label='Next Page']")
+            if not next_button or not next_button.is_enabled():
+                print("마지막 페이지입니다.")
+                break
+            next_button.click()
+            time.sleep(2)  # 페이지 로딩 대기
+    except NoSuchElementException:
+        print("다음 페이지 버튼을 찾을 수 없습니다.")
 
 # 크롤링할 페이지 URL 리스트
 url_list = [
@@ -48,11 +77,10 @@ for url in url_list:
     # 페이지 로드를 기다리기 위해 잠시 대기
     time.sleep(2)
 
-    # 페이지 소스를 출력 (여기서는 예시로 페이지 제목만 출력)
-    print(driver.title)
-
-    # 추가적인 데이터 추출이 필요한 경우, 여기에 코드 추가
+    # 페이지 네비게이션 시작
+    navigate_pages()
 
 # 웹 드라이버 종료
 driver.quit()
+
 
