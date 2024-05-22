bb63cb64ad631c6e88f09367e7d9cce8a3f01fb4
Choi seohyeon <csh200310@naver.com>
2024-05-22 13:22:23 +0900

    [fix] omid changer 변경

--- Citation/network/omid_doi_changer/comp_doi_omid.py
+++ Citation/network/omid_doi_changer/comp_doi_omid.py
@@ -4,25 +4,21 @@
 import pandas as pd
 import re
 
-# 파일을 읽어들입니다.
 paperlist_df = pd.read_csv('paperlist_doi.csv')
 omid_df = pd.read_csv('omid.csv')
 
-# 'omid.csv'의 'id' 열에서 DOI 값을 추출합니다.
-# 이때, 'doi:' 뒤의 값을 추출하며, 'doi'가 없는 경우는 제외합니다.
+# omid에 들어있는 값들이 모두 소문자였음. 그래서 소문자로 변환 해주고 해야 함
+paperlist_df['DOI'] = paperlist_df['DOI'].str.lower()
+
+# 'omid.csv'의 'id' 열에서 DOI 값을 추출
 omid_df['cleaned_doi'] = omid_df['id'].apply(lambda x: re.search(r'doi:(.+)', x, re.IGNORECASE).group(1).strip() if re.search(r'doi:(.+)', x, re.IGNORECASE) else None)
 
-# 결과를 확인하기 위해 불필요한 값이 없는지 확인합니다.
 print(omid_df[['omid', 'cleaned_doi']].dropna())
 
-# 'DOI' 열과 'cleaned_doi' 열을 기준으로 두 DataFrame을 조인합니다.
 result_df = pd.merge(paperlist_df, omid_df, left_on='DOI', right_on='cleaned_doi', how='inner')
 
-# 최종 결과에서 필요한 열만을 선택합니다.
 final_df = result_df[['DOI', 'omid']]
 
-# 결과 확인
 print(final_df)
 
-# 결과를 CSV 파일로 저장
 final_df.to_csv('matched_results.csv', index=False)
